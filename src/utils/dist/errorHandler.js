008ee0eb42f42efb5e5605f2c41fecfa87159678
AhsanLozaa <ahsanmohamed97@gmail.com>
2023-08-06 10:07:59 +0530

    updated the tsconfig file

---
+++ src/utils/dist/errorHandler.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.errorHandler = void 0;
+function errorHandler(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong' });
+}
+exports.errorHandler = errorHandler;
